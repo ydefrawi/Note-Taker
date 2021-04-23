09afd16afb7e81986071127050ff83a4d097946b
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-04-22 21:39:37 -0500

    got save and delete buttons to work

--- server.js
+++ server.js
@@ -27,11 +27,27 @@ app.get('/notes', function (req, res) {
 //--------
 
 //API Routes
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = 2;
+    notes.push(newNote);
+    return res.json(newNote);
+})
 
+app.get('/api/notes', (req, res) => res.json(notes));
 
-app.get('/api/notes', (req, res) => console.log(notes));
-
-
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    for(var i=0; i<notes.length; i++){
+        if(notes[i].id == id) {
+            notes.splice(i,1);
+            break;
+        }
+    }
+    // res.json(notes);
+    return res.json(notes)
+})
+    
 
 
 
